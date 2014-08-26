793be69f12c718bf7fcfd00cc316e796e5d4e49e
myriam <myriam@bluesteel>
2014-08-26 13:01:21 -0500

    Archivo de prueba >>Myriam Trujillo

---
+++ holaMyriam.php
@@ -0,0 +1,13 @@
+<?php
+echo 'Hola Mundo!';
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * }adflakdjfñladjflñksdjlñfkadjlñfk
+ */
+
+function hola($param) {
+    echo '>> ' . $param;
+}
+
+?>
